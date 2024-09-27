e4887af3354996c3cad6d36bbab294fe6164b8f6
Geetha <shangeetha@slatescience.com>
2024-09-27 12:51:09 +0530

    Commit Geetha

--- TestClass.ts
+++ TestClass.ts
@@ -1,24 +1,17 @@
 
 
-//Modified class async (params:type) => {
-    //Intl=1
-    
-    
-//}
-
-//රට අනුරට
-
-<<<<<<< HEAD
-//
-=======
-
-// Malsha Test 2
-//testQQQAA
 
 
-<<<<<<<<< Temporary merge branch 1
-//testQQQAA
 
-hello
+
+
+
+
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    hello
+}
 //testQa
->>>>>>> dd094c9ef17d4dc306e08d9931ae71e0941b8e96
+
