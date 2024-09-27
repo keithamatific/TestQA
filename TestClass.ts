c40028f5ea7951a498a756d700db1d4ebcee6a4d
Matific-amithwijesiri <amith@slatescience.com>
2024-09-27 12:41:36 +0530

    Resolved Conflicts

--- TestClass.ts
+++ TestClass.ts
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 
 Modified class testqa
 //Hello guys - from Afkar, Adding a conflict
-=======
 // Modified class
 // file edited by danuja 
 // Modified class testqa
 //Hello guys - from Afkar
->>>>>>> cc40330749df5378bb278696a62720e9b05c326c
-
 
 testqaa
 
-640bfe1becd619b84d24bfdf851f93bd5712b2ad
+// Malsha
 //testQQQAA
 
 
@@ -21,8 +17,6 @@ testqaa
 // Malsha Test 2
 //testQQQAA
 
-
-<<<<<<<<< Temporary merge branch 1
 //testQQQAA
 
 hello
